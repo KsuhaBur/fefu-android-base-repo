f2b55146eae5512c70cbd6e320912c266a26f935
KsuhaBur <burlakova.ka@students.dvfu.ru>
2021-11-07 20:25:41 +1000

    homework2

---
+++ app/src/main/java/ru/fefu/activitytracker/MainActivity.kt
@@ -0,0 +1,36 @@
+package ru.fefu.activitytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import ru.fefu.activitytracker.databinding.ActivityMainBinding
+import ru.fefu.activitytracker.fragments.ActivityFragment
+import ru.fefu.activitytracker.fragments.MyActivityFragment
+import ru.fefu.activitytracker.fragments.ProfileFragment
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.navigation_activity -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, ActivityFragment()).commit()
+                }
+                R.id.navigation_profile -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, ProfileFragment()).commit()
+                }
+            }
+            true
+        }
+    }
+
+
+}
