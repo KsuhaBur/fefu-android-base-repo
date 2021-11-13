f7f59067bb99fa57db91ce908b59b30ae531463b
KsuhaBur <burlakova.ka@students.dvfu.ru>
2021-11-13 14:21:51 +1000

    seem all

--- app/src/main/java/ru/fefu/activitytracker/MainActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.fefu.activitytracker.databinding.ActivityMainBinding
 import ru.fefu.activitytracker.fragments.ActivityFragment
 import ru.fefu.activitytracker.fragments.MyActivityFragment
@@ -11,22 +12,33 @@ import ru.fefu.activitytracker.fragments.ProfileFragment
 import java.lang.reflect.Array.newInstance
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val activityFragment = supportFragmentManager.findFragmentByTag("activityFragment")
+        val profileFragment = supportFragmentManager.findFragmentByTag("profileFragment")
+
         binding.bottomNav.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.navigation_activity -> {
-                    if (activityFragment !== null)
+                    if (activityFragment !== null) {
+                        if (profileFragment !== null) {
+                            supportFragmentManager.beginTransaction().hide(profileFragment).commit()
+                        }
                         supportFragmentManager.beginTransaction().show(activityFragment)
                             .commit()
+                    }
                     else {
+                        if (profileFragment !== null) {
+                            supportFragmentManager.beginTransaction().hide(profileFragment).commit()
+                        }
                         supportFragmentManager.beginTransaction().apply {
-                            add(
+                            replace(
                                 R.id.fragmentContainerView,
                                 ActivityFragment(),
                                 "activityFragment"
@@ -34,11 +46,31 @@ class MainActivity : AppCompatActivity() {
                             commit()
                         }
                     }
-                    }
+                }
                 R.id.navigation_profile -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragmentContainerView, ProfileFragment()).commit()
+                    if (profileFragment !== null) {
+                        if (activityFragment !== null) {
+                            supportFragmentManager.beginTransaction().hide(activityFragment)
+                                .commit()
+                        }
+                        supportFragmentManager.beginTransaction().show(profileFragment)
+                            .commit()
+                    }
+
+                    else {
+                        if (activityFragment !== null) {
+                            supportFragmentManager.beginTransaction().hide(activityFragment)
+                                .commit()
+                        }
+                        supportFragmentManager.beginTransaction().apply {
+                            replace(
+                                R.id.fragmentContainerView,
+                                ProfileFragment(),
+                                "profileFragment"
+                            )
+                            commit()
+                        }
+                    }
                 }
             }
             true
