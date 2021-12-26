600b4f7e8e65a0c8d7192eef7014cb3731f9f13d
KsuhaBur <burlakova.ka@students.dvfu.ru>
2021-12-26 15:59:33 +1000

    just commit

--- app/src/main/java/ru/fefu/activitytracker/main/WelcomeScreen.kt
+++ app/src/main/java/ru/fefu/activitytracker/main/WelcomeScreen.kt
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import ru.fefu.activitytracker.App
 import ru.fefu.activitytracker.R
+import ru.fefu.activitytracker.tracker.MainActivity
 
 class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (App.INSTANCE.sharedPrefs.getString("token", null) !== null) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
 
--- app/src/main/java/ru/fefu/activitytracker/tracker/ProfileFragment.kt
+++ app/src/main/java/ru/fefu/activitytracker/tracker/ProfileFragment.kt
@@ -1,16 +1,28 @@
 package ru.fefu.activitytracker.tracker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import ru.fefu.activitytracker.API.ProfileViewModel
 import ru.fefu.activitytracker.BaseFragment
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentProfileBinding
+import ru.fefu.activitytracker.API.Result
+import ru.fefu.activitytracker.API.User
+import ru.fefu.activitytracker.App
+import ru.fefu.activitytracker.main.WelcomeScreen
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
     private var _binding: FragmentProfileBinding? = null
     override val binding get() = _binding!!
+    private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +35,8 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_p
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
+
         binding.clickTextChangePassword.setOnClickListener {
             val manager = activity?.supportFragmentManager?.findFragmentByTag("profileFragment")?.childFragmentManager
             manager?.beginTransaction()?.apply {
@@ -35,6 +49,37 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_p
                 addToBackStack(null)
                 commit()
             }
+        }
+
+        viewModel.logoutUser
+            .onEach {
+                if (it is Result.Success<Unit>) {
+                    App.INSTANCE.sharedPrefs.edit().remove("token").apply()
+                    val intent = Intent(requireActivity(), WelcomeScreen::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                }
+                else if (it is Result.Errors<Unit>) {
+                    Toast.makeText(requireContext(), it.errors.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+            .launchIn(lifecycleScope)
+
+        viewModel.profile
+            .onEach {
+                if (it is Result.Success<User>) {
+                    binding.loginProfile.editText?.setText(it.result.login)
+                    binding.loginProfile.editText?.setText(it.result.name)
+                }
+                else if (it is Result.Errors<User>) {
+                    Toast.makeText(requireContext(), it.errors.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+            .launchIn(lifecycleScope)
+        viewModel.getProfile()
+
+        binding.buttonExit.setOnClickListener {
+            viewModel.logout()
         }
     }
 }
